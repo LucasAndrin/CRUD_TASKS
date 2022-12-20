b9f71553de1357bb9b851180f6af0398a829a175
LucasAndrinBCC <=>
2022-12-20 17:38:52 -0300

    Prepare for Login frontend

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
+import Login from '@/components/Login.vue'
+
 import Tasks from '@/views/Tasks.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/tasks',
       name: 'home',
       component: Tasks
     },
