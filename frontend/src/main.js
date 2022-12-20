f8bed36ba32a211df6e4580ec175f69072b2e244
LucasAndrinBCC <=>
2022-12-20 09:02:28 -0300

    prepare frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,22 @@
 import { createApp } from 'vue'
+
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueSweetalert2 from 'vue-sweetalert2'
 
-import './assets/main.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'sweetalert2/dist/sweetalert2.min.css'
+
+import './assets/scss/main.scss'
 
 const app = createApp(App)
 
+axios.defaults.baseURL = 'http://localhost:8000'
+
 app.use(router)
+app.use(VueAxios, axios)
+app.use(VueSweetalert2)
 
 app.mount('#app')
